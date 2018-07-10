b7552a3c81d1416df2267f93c792ed8a5b403ba7
Lane Rettig <lanerettig@gmail.com>
2018-07-10 14:42:50 +0200

    Initial layout

---
+++ assembly/ewasm.ts
@@ -0,0 +1,11 @@
+// Ewasm-AssemblyScript API
+// Author: Lane Rettig <lane@cryptonyc.org>
+
+export class Address {
+  constructor(
+    public Address: i32,
+  ) {}
+}
+
+// export type Address = i32
+
--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,22 @@
-// The entry file of your WebAssembly module.
+// Ewasm-AssemblyScript proof of concept multisig
+// Author: Lane Rettig <lane@cryptonyc.org>
+// DO NOT USE IN PRODUCTION. THIS CODE HAS NOT BEEN AUDITED.
+// All code offered without warranty.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+import { Address } from "./ewasm"
+
+// Create a new wallet. Takes a list of owners and a minimum required number
+// of signatures, returns address of new wallet contract.
+function create(owners: Address[], required: i32): Address {
+  wallet = new MultiSigWallet(owners, required)
+  return wallet
 }
+
+class MultiSigWallet {
+  constructor(
+    owners: Address[],
+    required: i32,
+  ) {}
+}
+
+
